4fa96e509ea91a8cd444e8714792dd17411c2246
Amitesh Gupta <amitesh.gupta@enbake.com>
2020-12-20 00:34:09 +0530

    Updates

---
+++ precache-manifest.e974fb8ebad72021853e5a320c103c0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98bec5f4beec2e96ebe7f21c9d22b464",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b75caf87c091e1f3589d",
+    "url": "/static/css/main.9a448b5b.chunk.css"
+  },
+  {
+    "revision": "b2731a8d69444660b0da",
+    "url": "/static/js/2.e8185ce0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e8185ce0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b75caf87c091e1f3589d",
+    "url": "/static/js/main.f9e50924.chunk.js"
+  },
+  {
+    "revision": "b10d08823a4df677ba67",
+    "url": "/static/js/runtime-main.de931e19.js"
+  }
+]);
